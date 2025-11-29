3b1f4c6760366acb5e5138fe5e5ebff40a6f155b
Yasin_ghanem <yassine.waleed@gmail.com>
2025-11-30 00:36:10 +0200

    changed 27verifier way

--- src/Mode27Verifier.java
+++ src/Mode27Verifier.java
@@ -1,101 +1,144 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-public class Mode27Verifier {
+public class Mode27Verifier implements Runnable {
 
     private int[][] sudoko;
-    private FinalDuplicate[] allDuplicates;
-    private DuplicateGetter getDuplicates;
 
     public Mode27Verifier(int[][] sudoko) {
         this.sudoko = sudoko;
-        this.allDuplicates = new FinalDuplicate[27];
-        this.getDuplicates = new DuplicateGetter(sudoko);
     }
 
-    public FinalDuplicate[] runCheck() throws InterruptedException {
+    @Override
+    public void run() {
+
+        CheckDuplicates check = new CheckDuplicates(sudoko);
+
+        List<Pair<String, Integer>> locations = new ArrayList<>();
+        List<Integer> duplicatedNumbers = new ArrayList<>();
+        List<List<Integer>> indices = new ArrayList<>();
+
         Thread[] threads = new Thread[27];
 
         for (int i = 0; i < 9; i++) {
-            threads[i] = new UnitThread(i, "ROW");
+            threads[i] = new RowThread(i, check, locations, duplicatedNumbers, indices);
         }
+
         for (int i = 0; i < 9; i++) {
-            threads[9 + i] = new UnitThread(i, "COLUMN");
+            threads[9 + i] = new ColumnThread(i, check, locations, duplicatedNumbers, indices);
         }
+
         for (int i = 0; i < 9; i++) {
-            threads[18 + i] = new UnitThread(i, "BOX");
+            threads[18 + i] = new BoxThread(i, check, locations, duplicatedNumbers, indices);
         }
 
-        for (Thread t : threads) t.start();
-        for (Thread t : threads) t.join();
+        for (Thread t : threads)
+            t.start();
 
-        return allDuplicates;
-    }
-
-    public void printResults() {
-        System.out.println("ROWS:");
-        printSection(0, 9);
-
-        System.out.println("----------------------------------");
-
-        System.out.println("COLUMNS:");
-        printSection(9, 18);
-
-        System.out.println("----------------------------------");
-
-        System.out.println("BOXES:");
-        printSection(18, 27);
-    }
-
-    private void printSection(int start, int end) {
-        for (int i = start; i < end; i++) {
-
-            FinalDuplicate fd = allDuplicates[i];
-
-            if (fd == null) continue;
-
-            for (Map.Entry<Integer, List<Integer>> entry : fd.getDuplicates().entrySet()) {
-
-                int number = entry.getKey();
-                List<Integer> positions = entry.getValue();
-
-                System.out.print(fd.getType() + " " + (fd.getIndex() + 1) + ", ");
-                System.out.print("#" + number + ", [");
-
-                for (int j = 0; j < positions.size(); j++) {
-                    System.out.print(positions.get(j) + 1);  // shift index to 1..9
-                    if (j < positions.size() - 1) System.out.print(", ");
-                }
-
-                System.out.println("]");
-            }
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException ignored) {}
         }
+
+        new PrintResult(locations, duplicatedNumbers, indices);
     }
 
-    private class UnitThread extends Thread {
+    private static class RowThread extends Thread {
         private int index;
-        private String type;
+        private CheckDuplicates check;
+        private List<Pair<String, Integer>> locations;
+        private List<Integer> duplicatedNumbers;
+        private List<List<Integer>> indices;
 
-        public UnitThread(int index, String type) {
+        public RowThread(int index, CheckDuplicates check,
+                         List<Pair<String, Integer>> locations,
+                         List<Integer> duplicatedNumbers,
+                         List<List<Integer>> indices) {
             this.index = index;
-            this.type = type;
+            this.check = check;
+            this.locations = locations;
+            this.duplicatedNumbers = duplicatedNumbers;
+            this.indices = indices;
         }
 
         public void run() {
-            FinalDuplicate fd;
+            ArrayList<Integer> row = check.getRow(index);
 
-            if (type.equals("ROW")) {
-                fd = getDuplicates.getRowDuplicate(index);
-                allDuplicates[index] = fd;
+            if (!check.isValid(row)) {
+                List<Integer> dups = check.getDuplicatedNumber(row);
+
+                for (int dup : dups) {
+                    locations.add(new Pair<>("ROW ", index + 1));
+                    duplicatedNumbers.add(dup);
+                    indices.add(check.getIndex(row, dup));
+                }
             }
-            else if (type.equals("COLUMN")) {
-                fd = getDuplicates.getColumnDuplicate(index);
-                allDuplicates[9 + index] = fd;
+        }
+    }
+
+    private static class ColumnThread extends Thread {
+        private int index;
+        private CheckDuplicates check;
+        private List<Pair<String, Integer>> locations;
+        private List<Integer> duplicatedNumbers;
+        private List<List<Integer>> indices;
+
+        public ColumnThread(int index, CheckDuplicates check,
+                            List<Pair<String, Integer>> locations,
+                            List<Integer> duplicatedNumbers,
+                            List<List<Integer>> indices) {
+            this.index = index;
+            this.check = check;
+            this.locations = locations;
+            this.duplicatedNumbers = duplicatedNumbers;
+            this.indices = indices;
+        }
+
+        public void run() {
+            ArrayList<Integer> col = check.getColumn(index);
+
+            if (!check.isValid(col)) {
+                List<Integer> dups = check.getDuplicatedNumber(col);
+
+                for (int dup : dups) {
+                    locations.add(new Pair<>("Column ", index + 1));
+                    duplicatedNumbers.add(dup);
+                    indices.add(check.getIndex(col, dup));
+                }
             }
-            else {
-                fd = getDuplicates.getBoxDuplicate(index);
-                allDuplicates[18 + index] = fd;
+        }
+    }
+
+    private static class BoxThread extends Thread {
+        private int index;
+        private CheckDuplicates check;
+        private List<Pair<String, Integer>> locations;
+        private List<Integer> duplicatedNumbers;
+        private List<List<Integer>> indices;
+
+        public BoxThread(int index, CheckDuplicates check,
+                         List<Pair<String, Integer>> locations,
+                         List<Integer> duplicatedNumbers,
+                         List<List<Integer>> indices) {
+            this.index = index;
+            this.check = check;
+            this.locations = locations;
+            this.duplicatedNumbers = duplicatedNumbers;
+            this.indices = indices;
+        }
+
+        public void run() {
+            ArrayList<Integer> box = check.getBox(index);
+
+            if (!check.isValid(box)) {
+                List<Integer> dups = check.getDuplicatedNumber(box);
+
+                for (int dup : dups) {
+                    locations.add(new Pair<>("Box ", index + 1));
+                    duplicatedNumbers.add(dup);
+                    indices.add(check.getIndex(box, dup));
+                }
             }
         }
     }
