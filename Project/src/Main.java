9f599ce3083148d6afda549a0cd8f7629aaf7518
amlegomaa <amlegomaa22@gmail.com>
2025-11-29 03:06:49 +0200

    print class

---
+++ Project/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+void main() {
+
+//    List<Pair<String, Integer>> locations = new ArrayList<>();
+//    locations.add(new Pair<>("Row", 1));
+//    locations.add(new Pair<>("Row", 2));
+//
+//    locations.add(new Pair<>("Column", 3));
+//    locations.add(new Pair<>("Box", 1));
+//
+//    List<List<Integer>> results = new ArrayList<>();
+//    results.add(Arrays.asList(2,3));
+//    results.add(Arrays.asList(2,5,7));
+//    results.add(Arrays.asList(1,6));
+//    results.add(Arrays.asList(2,3));
+//
+//    PrintResult printResult = new PrintResult(locations, 3, results);
+
+
+
+
+}
